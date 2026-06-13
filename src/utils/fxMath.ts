de421f3c10e028098773ef1d46a2228e0c7d3705
Josh <15219491+jkaps9@users.noreply.github.com>
2026-06-12 22:10:02 -0400

    move tolocalestring out of math and into form component

--- src/utils/fxMath.ts
+++ src/utils/fxMath.ts
@@ -1,7 +1,3 @@
 export function convertAmount(amount: number, rate: number) {
-  const result = amount * rate;
-  return result.toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+  return amount * rate;
 }
