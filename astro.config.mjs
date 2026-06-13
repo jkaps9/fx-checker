7be091068d3c5476d1291286fedbdb7067d3fbc6
Josh <15219491+jkaps9@users.noreply.github.com>
2026-06-13 11:32:42 -0400

    add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com", //TODO: update me
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "JetBrains Mono",
+      cssVariable: "--font-jetbrains-mono",
+      weights: [400, 500, 700],
+    },
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "/src/styles/_variables.scss" as *;
+          @use "/src/styles/_mixins.scss" as *;
+          `,
+        },
+      },
+    },
+  },
+});
