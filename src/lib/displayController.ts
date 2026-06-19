e28b053fbbaab22c888ae84a275dda73ca695da0
Josh <15219491+jkaps9@users.noreply.github.com>
2026-06-19 14:36:11 -0400

    move update of output to the displayController

--- src/lib/displayController.ts
+++ src/lib/displayController.ts
@@ -1,8 +1,13 @@
 import { APIController } from "./apiController";
+import { convertAmount } from "@utils/fxMath";
 
 const displayController = (function () {
   const form = document.getElementById("fx-input") as HTMLFormElement;
   const apiController = new APIController();
+  const baseAmount = document.getElementById("base-amount") as HTMLInputElement;
+  const outputAmount = document.getElementById(
+    "output-amount",
+  ) as HTMLOutputElement;
   const results = document.getElementById("output") as HTMLParagraphElement;
 
   const initizalize = () => {
@@ -17,11 +22,30 @@ const displayController = (function () {
 
       if (!base || !target || base === target) return;
       updateBaseConversion("Fetching rates...");
+      apiController.search(base, target).then((data) => {
+        if (data) {
+          updateBaseConversion(
+            `1 ${data.base} = ${data.rate.toFixed(4)} ${data.quote}`,
+          );
+          const baseAmt = baseAmount.valueAsNumber;
+          if (baseAmt) updateTargetAmount(baseAmt, data.rate);
+        } else {
+          updateBaseConversion("Error fetching rates. Please try again.");
+        }
+      });
     });
   };
 
   const updateBaseConversion = (str: string) => {
     results.textContent = str;
+  };
+
+  const updateTargetAmount = (amount: number, rate: number) => {
+    const result = convertAmount(amount, rate);
+    outputAmount.value = result.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
   };
 
   return { initizalize };
