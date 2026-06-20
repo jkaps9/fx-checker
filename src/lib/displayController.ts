24a450b9475be13e3317d85cc75742e6ea15560f
Josh <15219491+jkaps9@users.noreply.github.com>
2026-06-19 22:32:16 -0400

    move currency switcher to dom controller

--- src/lib/displayController.ts
+++ src/lib/displayController.ts
@@ -1,5 +1,6 @@
 import { APIController } from "./apiController";
 import { convertAmount } from "@utils/fxMath";
+import storageManager from "./storageManager";
 
 const displayController = (function () {
   const form = document.getElementById("fx-input") as HTMLFormElement;
@@ -9,19 +10,30 @@ const displayController = (function () {
     "output-amount",
   ) as HTMLOutputElement;
   const results = document.getElementById("output") as HTMLParagraphElement;
+  const favoriteButton = document.getElementById(
+    "favorite-button",
+  ) as HTMLButtonElement;
+  const currencySwapBtn = document.getElementById(
+    "currency-swap",
+  ) as HTMLButtonElement;
 
   const initizalize = () => {
+    const formData = new FormData(form);
+    const base: string = formData.get("base")?.toString() ?? "";
+    const target: string = formData.get("target")?.toString() ?? "";
+    updateFavoriteButtonState(base, target);
+
     form.addEventListener("submit", async (e) => {
       e.preventDefault();
     });
 
     form.addEventListener("input", () => {
       const formData = new FormData(form);
-      const base = formData.get("base")?.toString();
-      const target = formData.get("target")?.toString();
-
+      const base: string = formData.get("base")?.toString() ?? "";
+      const target: string = formData.get("target")?.toString() ?? "";
       if (!base || !target || base === target) return;
       updateBaseConversion("Fetching rates...");
+      updateFavoriteButtonState(base, target);
       apiController.search(base, target).then((data) => {
         if (data) {
           updateBaseConversion(
@@ -33,6 +45,13 @@ const displayController = (function () {
           updateBaseConversion("Error fetching rates. Please try again.");
         }
       });
+    });
+
+    currencySwapBtn.addEventListener("click", () => {
+      const formData = new FormData(form);
+      const base: string = formData.get("base")?.toString() ?? "";
+      const target: string = formData.get("target")?.toString() ?? "";
+      swapCurrencies(base, target);
     });
   };
 
@@ -46,6 +65,33 @@ const displayController = (function () {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
+  };
+
+  const updateFavoriteButtonState = (base: string, target: string) => {
+    if (base && target && base !== "" && target !== "") {
+      if (storageManager.hasFavorite(base, target)) {
+        favoriteButton.classList.add("active");
+      } else {
+        favoriteButton.classList.remove("active");
+      }
+    }
+  };
+
+  const swapCurrencies = (base: string, target: string) => {
+    console.log(`swapping ${base} and ${target}`);
+    const selectBase = document.getElementById(
+      "base-currency",
+    ) as HTMLSelectElement;
+    const selectTarget = document.getElementById(
+      "target-currency",
+    ) as HTMLSelectElement;
+
+    selectBase.value = target;
+    selectTarget.value = base;
+
+    const targetVal = Number(outputAmount.value.replace(",", ""));
+
+    baseAmount.valueAsNumber = targetVal;
   };
 
   return { initizalize };
